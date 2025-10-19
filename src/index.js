65e4226e2a3a758472d3afe30ab8db095007ced2
KarinaV-cyber <virtualkarina2553@gmail.com>
2025-10-18 23:18:56 -0400

    Added Axios and setup AI API integration

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
+function displayPoem(response){
+    console.log("poem generated");
+// response.data.answer
+new Typewriter("#poem", {
+  strings: response.data.answer,
+   delay:1,
+   cursor:"🌟",
+   autoStart: true,
+});
+}
+
+
 function generatePoem(event){
     event.preventDefault ();
 
-new Typewriter('#poem', {
-  strings: "Que par la douleur du partir Aimer a perdre la raison",
-   autoStart: true,
-   delay:1,
-   cursor:"",
-});
- 
-    //alert("Generating Poem");
+    //build the API URL
+    //Make a call to the API using Axios
+    //diplay the generated poem
+     let instructionsInput=document.querySelector("#user-instructions");
+
+    let apiKey="oa13410f1922d7b4t12b44ae83ead081";
+        let context="You are a romantic love expert and love to write short poems. Your mission is to generate a 4 line poem  in basic HTML . Separate each line with a <br/>.  Sign the poem with `karina Smith` inside a <strong> element, at the end of the poem. Make sure to follow the user instructions.";
+        let prompt=`User-instructions: Generate a french poem about ${instructionsInput.value}`;
+    let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("generating poem");
+    console.log(`Prompt: ${prompt}`);
+    console.log (`Context: ${context}`);
+
+axios.get(apiUrl).then(displayPoem);
+
+     //alert("Generating Poem");
 }
 
 
